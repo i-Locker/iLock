8a06b86d39abc72e85e5aa70c22dc6a457af45d2
Interchained <president@worldvaporexpo.com>
2023-04-18 12:46:27 -0400

    genesis

--- serve.js
+++ serve.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const morgan = require('morgan');
+var logger = require('morgan');
 const path = require('path');
+const fs = require('fs');
 const app = express();
-
+app.use(morgan('common'));
 app.use(morgan(':method :url :status :http-version :response-time '));
+app.use(logger('common', {
+    stream: fs.createWriteStream('../access.log', {flags: 'a'})
+}));
+app.use(logger('dev'));
 app.use("/", serveStatic ( path.join (__dirname, '/build') ) );
 app.get('*', function (req, res) {
 res.sendFile(__dirname + '/build/index.html')
